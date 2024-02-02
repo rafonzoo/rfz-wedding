edf3da05ce97b62687869164bc0f9797cf38995b
Rafa <rafa.rahmandika@cermati.com>
2024-02-02 12:19:04 +0700

    fix sheet non modal open twice

--- components/BottomSheet/index.tsx
+++ components/BottomSheet/index.tsx
@@ -185,7 +185,7 @@ const BottomSheet: RFZ<BottomSheetProps> = ({
 
     content?.onCloseAutoFocus?.(e)
 
-    if (!e.defaultPrevented && !root?.modal) {
+    if (!e.defaultPrevented) {
       focusRef.current?.focus()
     }
   }
@@ -285,7 +285,7 @@ const BottomSheet: RFZ<BottomSheetProps> = ({
           style={{ zIndex: `${999 + sheetIndex}` }}
           className={tw(
             'fixed bottom-0 left-0 right-0 z-[888] flex max-h-[min(906px,96%)] outline-none',
-            !root?.modal && 'data-[state=closed]:animate-dialog-hide',
+            'data-[state=closed]:animate-dialog-hide',
             content?.className
           )}
           onOpenAutoFocus={onOpenAutoFocus}
